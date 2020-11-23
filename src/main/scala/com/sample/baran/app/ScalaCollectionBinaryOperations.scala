96331f84829a3f09aff305d9e7b52211dd3eda65
Barandeep Singh <barandeepsingh@gmail.com>
2020-11-23 20:32:47 +0530

    Renamed object and changed package
    Renamed object

--- src/main/scala/com/sample/baran/app/ScalaCollectionBinaryOperations.scala
+++ src/main/scala/com/sample/baran/app/ScalaCollectionBinaryOperations.scala
@@ -1,22 +1,24 @@
-object ScalaOperations extends App{
+package com.sample.baran.app
+
+object ScalaCollectionBinaryOperations extends App {
   //Reduce - Apply a binary operator on all elements of Collection. Return the final value after performing operation on all elements of the collection.
   //Fold - Apply a binary operator on all elements of Collection. Start with the initial value. Return the final value after performing operation on all elements of the collection.
   //Scan - Apply a binary operator on all elements of Collection. Start with the initial value. It return a map after every operation.
 
-  val elements = Seq(1,2,3,4)
+  val elements = Seq(1, 2, 3, 4)
 
-  println(elements.reduceLeft((x,y)=>{
-    println("x is "+x +" and y is "+y)
-    x+y
+  println(elements.reduceLeft((x, y) => {
+    println("x is " + x + " and y is " + y)
+    x + y
   }))
 
-  println(elements.reduceRight((x,y)=>{
-    println("x is "+x +" and y is "+y)
-    x+y
+  println(elements.reduceRight((x, y) => {
+    println("x is " + x + " and y is " + y)
+    x + y
   }))
 
-  println(elements.foldLeft(100)((x,y)=>{
-    println("x is "+x+" and y is "+y)
-    x+y
+  println(elements.foldLeft(100)((x, y) => {
+    println("x is " + x + " and y is " + y)
+    x + y
   }))
 }
